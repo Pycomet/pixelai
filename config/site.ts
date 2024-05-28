0cd2a08118ac4ee6f28a4987c614c615cfce0c17
Alfred Emmanuel <118021171+Pycomet@users.noreply.github.com>
2024-05-28 01:14:57 +0100

    setup base

---
+++ config/site.ts
@@ -0,0 +1,31 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "PixelAI - AI Thumbnail Generator",
+  description: "Automatically create engaging thumbnails for online content using advanced language models and image processing. Streamlines the thumbnail creation workflow and enhances visual presentation to boost engagement.",
+  navItems: [
+    // {
+    //   label: "Home",
+    //   href: "/",
+    // },
+    // {
+    //   label: "Pricing",
+    //   href: "/pricing",
+    // },
+  ],
+  navMenuItems: [
+    // {
+    //   label: "Home",
+    //   href: "/",
+    // },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/pycomet",
+    docs: "https://github.com/pycomet",
+    sponsor: "https://patreon.com/jrgarciadev", // TODO: Add Patreon Account
+  },
+};
