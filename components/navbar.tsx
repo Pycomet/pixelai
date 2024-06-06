14a3c5a24735d6e8a3a625f4fd4009b591f3a324
Alfred Emmanuel <118021171+Pycomet@users.noreply.github.com>
2024-06-06 02:00:24 +0100

    add headers config

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -63,7 +63,7 @@ export const Navbar = () => {
         <NavbarItem className="hidden md:flex">
           <Button
             as={Link}
-            className="bg-gradient-to-tr from-yellow-500 dark:from-pink-500 dark:to-yellow-500 to-pink-500 text-white shadow-lg"
+            className="bg-gradient-to-tr from-yellow-500 dark:from-pink-500 dark:to-yellow-500 to-pink-500 text-sm font-normal"
           >
             Sign In
           </Button>
